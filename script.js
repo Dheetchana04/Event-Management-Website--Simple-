dd937d559cdcb9b8fbcfa99fb1cb0814d39f8c74
Dheetchana04 <152024382+Dheetchana04@users.noreply.github.com>
2026-01-27 12:13:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function signup() {
+    alert("Account created successfully!");
+    window.location.href = "login.html";
+}
+
+function login() {
+    alert("Login successful!");
+    window.location.href = "index.html";
+}
+
+function success() {
+    alert("Event Registered Successfully!");
+}
